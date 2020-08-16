7b09fb2334afab1dc2fd334359103e30fb26101b
Felicitas Deutges <deutges.ironhack@gmail.com>
2020-08-16 19:52:25 +0200

    test debug

--- server/configs/queries.js
+++ server/configs/queries.js
@@ -10,9 +10,10 @@ const pool = new Pool({
 const getUsers = (request, response) => {
   pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
     if (error) {
-      throw error
+      response.status(200).json(["hello"])
+    } else {
+      response.status(200).json(results.rows)
     }
-    response.status(200).json(results.rows)
   })
 }
 module.exports = {
