e2fd771471aec6dda122c5fb71bb9972905d8c5d
Felicitas Deutges <deutges.ironhack@gmail.com>
2020-08-16 19:56:09 +0200

    debug error

--- server/configs/queries.js
+++ server/configs/queries.js
@@ -10,7 +10,7 @@ const pool = new Pool({
 const getUsers = (request, response) => {
   pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
     if (error) {
-      response.status(200).json(["hello"])
+      response.status(200).json(["hello", error])
     } else {
       response.status(200).json(results.rows)
     }
