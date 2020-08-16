33e522b9eedfae46b2dbe21a51f5141da0244da6
Felicitas Deutges <deutges.ironhack@gmail.com>
2020-08-16 20:56:58 +0200

    security

--- server/configs/queries.js
+++ server/configs/queries.js
@@ -7,7 +7,7 @@ const client = new Client({
 
 const getUsers = (request, response) => {
   client.connect();
-  client.query('SELECT * FROM users ORDER BY id ASC;', (err, res) => {
+  client.query('SELECT id,username,email FROM users ORDER BY id ASC;', (err, res) => {
     if (err) response.status(200).json(["hello",err])
     else response.status(200).json(res.rows)
     client.end();
