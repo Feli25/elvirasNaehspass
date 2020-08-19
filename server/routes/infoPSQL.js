c4b8843a3ca25e9827c70e76886e1e593a058042
Felicitas Deutges <deutges.ironhack@gmail.com>
2020-08-19 23:23:25 +0200

    change the info to use the psql

--- server/routes/infoPSQL.js
+++ server/routes/infoPSQL.js
@@ -5,10 +5,10 @@ const Info = require('../models/Info')
 // const cloudinary = require('cloudinary');
 
 // DB structure/model:
-// id, header, content, imgpath, imgname, publicid
+// id, category, header, content, list, teacher
 
 // ALWAYS export it this way though:
-// _id, header, content, imgPath, imgName, public_id
+// _id, category, header, content, list, teacher
 // this is what the frontend is expecting
 
 const { Client } = require('pg');
@@ -17,144 +17,131 @@ const configs = {
   ssl: false,
 }
 
-router.get('/kurse', (req,res,next)=>{
-  Info.find({ category: "KURSE" })
-  .then(info =>{
-    res.json(info)
-  })
-  .catch(err => next(err))
-})
+function convertStringToJson (string) {
+  var newJson = string.replace(/'/g, '"');
 
-router.get('/workshops', (req,res,next)=>{
-  Info.find({ category: "WORKSHOPS" })
-  .then(info =>{
-    res.json(info)
-  })
-  .catch(err => next(err))
-})
+  newJson = newJson.replace(/([^"]+)|("[^"]+")/g, function($0, $1, $2) {
+      if ($1) {
+          return $1.replace(/([a-zA-Z0-9]+?):/g, '"$1":');
+      } else {
+          return $2; 
+      } 
+  });
 
-router.get('/dessous', (req,res,next)=>{
-  Info.find({ category: "DESSOUS" })
-  .then(info =>{
-    res.json(info)
-  })
-  .catch(err => next(err))
-})
+  return JSON.parse(newJson);
+}
 
-router.get('/table', (req,res,next)=>{
-  Info.find({ category: "TABLE" })
-  .then(info =>{
-    res.json(info)
-  })
-  .catch(err => next(err))
-})
-
-// router.post('/new-pic',  parser.single('picture'),(req,res,next)=>{
-//   let { header, content, category, list } = req.body
-//   let file = req.file
-//   console.log("list",list)
-//   Info.create({
-//     header:header,
-//     content:content,
-//     list:list,
-//     category:category,
-//     imgPath:file.url,
-//     imgName:file.originalname,
-//     public_id:file.public_id
-//   })
-//   .then(info => {
-//     res.json({
-//       success: true,
-//       info
-//     });
-//   })
-//   .catch(err => console.log(err))
-// })
-
-router.get('/byid/:id', (req,res,next)=>{
-  let id = req.params.id
-  Info.findById(id)
-  .then(info =>{
-    res.json(info)
-  })
-  .catch(err => next(err))
-})
-
-router.post('/new', (req,res,next)=>{
-  let { header, content, category, list,teacher } = req.body
-  Info.create({
-    header:header,
-    content:content,
-    list:list,
-    category:category,
-    imgPath:null,
-    imgName:null,
-    public_id:null,
-    teacher:teacher
-  })
-  .then(info => {
-    res.json({
-      success: true,
-      info
-    });
-  })
-  .catch(err => console.log(err))
-})
-
-// router.post('/edit-pic/:id', parser.single('picture'), (req,res,next)=>{
-//   let id = req.params.id
-//   let { header, content, list, category, public_id } = req.body
-//   cloudinary.v2.uploader.destroy(public_id, function(result) { console.log(result) });
-//   let file = req.file;
-
-//   Info.findByIdAndUpdate(id,{
-//     header:header,
-//     content:content,
-//     list:list,
-//     category:category,
-//     imgName:file.originalname,
-//     imgPath:file.url,
-//     public_id: file.public_id
-//   })
-//     .then(info => {
-//       res.json({
-//         success: true,
-//       })
-//     })
-//     .catch(err=>{console.log(err)})
-// })
-
-router.post('/edit/:id', (req,res,next)=>{
-  let id = req.params.id
-  let { header, content, list, category,teacher } = req.body
+router.get('/', async (req,res,next)=>{
+  try{
+    const client = new Client(configs);
+    client.connect();
   
-  Info.findByIdAndUpdate(id,{
-    header:header,
-    content:content,
-    list:list,
-    category:category,
-    teacher:teacher
-  })
-    .then(info => {
-      res.json({
-        success: true,
-      })
+    const infoQuery = await client.query('SELECT id AS _id, category, header, content, list AS liststring, teacher FROM infos')
+    const info = infoQuery.rows.map(kurs => {
+      let list = convertStringToJson(kurs.liststring)
+      return {...kurs, list: list}
     })
-    .catch(err=>{console.log(err)})
+    res.json( info )
+    client.end();
+  } catch(err){
+    next(err)
+  }
 })
 
-router.get('/delete/:id', (req,res,next)=>{
-  let id = req.params.id
-      Info.findByIdAndDelete(id)
-      .then(sth=>{
-        res.json({
-          success:true
-        })
-      })
-      .catch(err=>{
-        console.log(err)
-      })
+router.get('/byid/:id', async (req,res,next)=>{
+  try{
+    const client = new Client(configs);
+    client.connect();
+    let id = req.params.id
+  
+    const infoQuery = await client.query('SELECT id AS _id, category, header, content, list AS liststring, teacher FROM infos WHERE id=$1',[id])
+    const info = infoQuery.rows.map(kurs => {
+      let list = convertStringToJson(kurs.liststring)
+      return {...kurs, list: list}
+    })
+    res.json( info )
+    client.end();
+  } catch(err){
+    next(err)
+  }
 })
+
+router.get('/delete/:id', async (req,res,next)=>{
+  try{
+    const client = new Client(configs);
+    client.connect();
+    let id = req.params.id
+  
+    const deletePost = await client.query('DELETE FROM infos WHERE id=$1', [id])
+    if(!deletePost) throw "ERROR";
+    res.json({ success:true })
+    client.end();
+  } catch(err){
+    next(err)
+  }
+})
+
+router.get('/:category', async (req,res,next)=>{
+  try{
+    const client = new Client(configs);
+    client.connect();
+    let category = req.params.category.toUpperCase()
+  
+    const tableQuery = await client.query('SELECT id AS _id, category, header, content, list AS liststring, teacher FROM infos WHERE category=$1',[category])
+    const table = tableQuery.rows.map(kurs => {
+      let list = convertStringToJson(kurs.liststring)
+      return {...kurs, list: list}
+    })
+    res.json( table )
+    client.end();
+  } catch(err){
+    next(err)
+  }
+})
+
+router.post('/new', async (req,res,next)=>{
+  try{
+    const client = new Client(configs);
+    client.connect();
+    let { header, content, category, list,teacher } = req.body
+    const insertedPost = await client.query('INSERT INTO infos (category, header, content, list, teacher) VALUES($1,$2,$3,$4,$5)',[
+      category,
+      header,
+      content,
+      JSON.stringify(list),
+      teacher
+    ])
+    if(!insertedPost) throw "ERROR";
+    res.json({ success:true })
+    client.end();
+  } catch(err){
+    next(err)
+  }
+})
+
+router.post('/edit/:id', async (req,res,next)=>{
+  try{
+    const client = new Client(configs);
+    client.connect();
+    let id = req.params.id
+    let { header, content, list, category,teacher } = req.body
+  
+    const updatedPost = await client.query('UPDATE infos SET category=$1, header=$2, content=$3, list=$4, teacher=$5 WHERE id=$6',[
+      category,
+      header,
+      content,
+      JSON.stringify(list),
+      teacher,
+      id
+    ])
+    if(!updatedPost) throw "ERROR";
+    res.json({ success:true })
+    client.end();
+  } catch(err){
+    next(err)
+  }
+})
+
 
 
 module.exports = router;
